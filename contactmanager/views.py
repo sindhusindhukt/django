98b2b55fd446a8eaf0ae126edbff743bed870c94
sindhusindhukt <ktamsindhu@gmail.com>
2024-11-16 21:17:34 +0530

    initial commit

---
+++ contactmanager/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Contact
+from .serializers import ContactSerializer
+
+class ContactView(viewsets.ModelViewSet):
+	queryset = Contact.objects.all()
+	serializer_class = ContactSerializer
